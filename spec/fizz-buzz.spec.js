d9a600ce8d8e50cc24cd4d2c3094b65de04e7d25
AndreasMN <andycutter@godmail.dk>
2018-06-24 15:29:35 +0200

    add fizz buzz source file and made test for function returning number pass

---
+++ spec/fizz-buzz.spec.js
@@ -0,0 +1,9 @@
+const { FizzBuzz } = require('./spec.helper')
+
+describe('Fizz Buzz', () => {
+
+    it('returns number if no game rules are met', () => {
+        let fizzBuzz = new FizzBuzz
+        expect(fizzBuzz.check(1)).to.eql(1)
+    })
+})
