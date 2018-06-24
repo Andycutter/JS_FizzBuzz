4f169e567af548221a5364859ac7dfe632938d23
AndreasMN <andycutter@godmail.dk>
2018-06-24 17:34:15 +0200

    add index.html and corrected typos to make test pass and index.html work

--- features/index.feature.js
+++ features/index.feature.js
@@ -11,16 +11,16 @@ describe('User can input a value and get FizzBuzz results', () => {
 
     beforeEach(async () => {
         await browser.page.reload();
-    });
+    })
 
     after(async () => {
         await browser.close();
     })
 
-    it('When we click on the "Check"-button', async () => {
-        await browser.fillIn("input[id='value']", {with: 3})
+    it('When we click on the "Check" button', async () => {
+        await browser.fillIn("input[id='value']", { with: "3" })
         await browser.clickOnButton("input[value='Check']")
-        let content = await browser.getContent("[id='displau_answer']")
+        let content = await browser.getContent("[id='display_answer']")
         expect(content).to.eql('Fizz');
     })
 })
